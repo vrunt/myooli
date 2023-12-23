64541809755e5aac2720e7fb840facddbf74c907
vrunt <vrunt@panix.com>
2023-12-23 02:29:36 -0500

    font change

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,13 +4,16 @@ export default defineApp((req, ctx) => {
   return (
     <html>
       <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Mulish:wght@200&display=Paytone+One&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mulish:wght@200&family=Paytone+One&display=swap" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Myooli</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body>
-        <h1 class="text-4xl font-bold">myooli</h1>
+      <body class="yp-4" style="font-family: 'Mulish', sans-serif">
+        <h1 class="text-4xl font-bold" style="font-family: 'Paytone One', sans-serif">myooli</h1>
         <ctx.Component />
       </body>
     </html>
